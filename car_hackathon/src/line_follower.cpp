e12004e2bca585ff7acc650985b9ffe44dc143b5
Ramir Sultanov <ramirrsultanov@stud.kpfu.ru>
2024-05-29 23:36:49 +0300

    made pid regulator

--- car_hackathon/src/line_follower.cpp
+++ car_hackathon/src/line_follower.cpp
@@ -30,6 +30,53 @@ void image_callback(const sensor_msgs::ImageConstPtr& msg)
 {        
     cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
     cv::Mat image = cv_ptr->image;
+    
+    cv::Mat hsvImage;
+    cv:cvtColor(image, hsvImage, CV_BGR2HSV);
+
+    // Threshold the HSV image, keep only the yellow pixels
+    cv::Mat mask;
+    cv::Scalar lower_red(0, 100, 100);
+    cv::Scalar upper_red(10, 255, 255);
+    cv::inRange(hsvImage, lower_red, upper_red, mask);
+
+    int width = mask.cols;
+    int height = mask.rows;
+
+    int search_top = 7 * height / 8;
+    int search_bottom = height;
+
+    // Zero out pixels outside the desired region
+    for (int y = 0; y < height - 2; y++) {
+        if (y < search_top || y > search_bottom) {
+            for (int x = 0; x < width; x++) {
+                mask.at<cv::Vec3b>(y, x)[0] = 0;
+                mask.at<cv::Vec3b>(y, x)[1] = 0;
+                mask.at<cv::Vec3b>(y, x)[2] = 0;
+            }
+        }
+    }
+
+    // Use the moments() function to calculate the centroid of the blob of the binary image
+    cv::Moments M = cv::moments(mask);
+
+    if (M.m00 > 0) {
+        int cx = int(M.m10 / M.m00);
+        int cy = int(M.m01 / M.m00);
+        cv::circle(image, cv::Point(cx, cy), 20, CV_RGB(255, 0, 0), -1);
+
+        // Move the robot in proportion to the error signal
+        int err = cx - width / 2;
+        //geometry_msgs::Twist cmd;
+        //cmd.linear.x = 0.1;
+        //cmd.angular.z = -(float)err / 500;
+        const float angle = -err / 10;
+        const float wheelDist = 0.2;
+        const float speed = 12;
+        const float right = (angle * wheelDist) / 2 + speed;
+float left = speed * 2 - right;
+        motors(left, right);
+    }
 
     // TODO: control the motors
     // motors(50,50);
